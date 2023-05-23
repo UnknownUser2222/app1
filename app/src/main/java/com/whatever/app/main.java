70c734258c3ea0f21eaccbc8b931147977476ac3
UnknownUser2222 <userunamed981@gmail.com>
2023-05-23 12:10:54 +0600

    Initial commit

---
+++ app/src/main/java/com/whatever/app/main.java
@@ -0,0 +1,17 @@
+package com.whatever.app;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
+public class main {
+// get the textview, then
+
+    int min = 1;
+    int max = 10;
+    Random r = new Random();
+    int i1 = r.nextInt(max - min + 1) + min;
+
+
+}
